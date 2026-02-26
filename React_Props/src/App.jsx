0f9dc59da2d50787231228b66fe1c4739ca0b279
Tushar-WebDev-ai <tushar50sharma1415@gmail.com>
2026-02-27 02:41:40 +0530

    practice on props

---
+++ React_Props/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-blue-400 text-white p-10 rounded-xl mb-4'>Tailwind testing</h1> 
+      <Card username="Tusharr"/>
+      <Card username="Toshi"/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
