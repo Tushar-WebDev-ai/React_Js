9f8039b97d1035164231e52cbca4aa36d34bf52e
Tushar-WebDev-ai <tushar50sharma1415@gmail.com>
2026-02-27 02:42:44 +0530

    moving forward

--- React_Props/src/App.jsx
+++ React_Props/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <h1 className='bg-blue-400 text-white p-10 rounded-xl mb-4'>Tailwind testing</h1> 
       <Card username="Tusharr"/>
-      <Card username="Toshi"/>
+      <Card username="Tusha"/>
       <Card/>
     </>
   )
